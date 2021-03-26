70a0c95876753364f2da83ae33106cf21df0acee
Naveen <chhaprohconsultancy@gmail.com>
2021-03-26 21:50:20 +0530

    user-article-associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_many :articles
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true,
     uniqueness: { casesensitive: false }, length: {minimum: 3, maximum: 40 }
   
