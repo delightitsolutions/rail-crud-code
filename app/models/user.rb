dadf178ac97db21bc0b060814b83255ae180c58c
Naveen <chhaprohconsultancy@gmail.com>
2021-03-26 19:14:47 +0530

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-
+  validates :username, presence: true,
+    uniqueness: { casesensitive: false }, length: {minimum: 3, maximum: 40 }
+  
+  validates :email, presence: true,
+    uniqueness: { casesensitive: false }, length: {maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
